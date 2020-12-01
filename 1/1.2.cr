2f9031e0ca86506fe9a66ba145e51fcf4a46a359
Ary Borenszweig <asterite@gmail.com>
2020-12-01 18:36:58 -0300

    Use combination instead of permutation

--- 1/1.2.cr
+++ 1/1.2.cr
@@ -1,6 +1,6 @@
 puts File.read_lines("#{__DIR__}/input.txt")
   .map(&.to_i)
-  .each_permutation(3, reuse: true)
+  .each_combination(3, reuse: true)
   .find { |array| array.sum == 2020 }
   .not_nil!
   .product
