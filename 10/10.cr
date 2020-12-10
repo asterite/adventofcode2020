1c29e762677710244ceae88037c6564c7bb11553
Ary Borenszweig <asterite@gmail.com>
2020-12-10 07:11:35 -0300

    Unify day 10 in a single file

--- 10/10.cr
+++ 10/10.cr
@@ -2,6 +2,9 @@ numbers = File.read_lines("#{__DIR__}/input.txt").map(&.to_i).sort
 numbers.unshift(0)
 numbers.push(numbers.max + 3)
 
+counts = numbers.each.cons_pair.map { |(x, y)| y - x }.tally
+puts "Part 1: #{counts[1] * counts[3]}"
+
 nexts = numbers[...-1].each_with_index.to_h do |(n, i)|
   {n, numbers[i + 1..].take_while { |m| m - n <= 3 }}
 end
@@ -11,4 +14,4 @@ nexts.keys.reverse_each do |n|
   possiblities[n] = nexts[n].sum { |m| possiblities[m] }
 end
 
-puts possiblities[0]
+puts "Part 2: #{possiblities[0]}"
