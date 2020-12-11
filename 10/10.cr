b1f54cfb29bf3e30034c4cc4336909c0c49196aa
Ary Borenszweig <asterite@gmail.com>
2020-12-11 08:38:50 -0300

    Revert "Day 10"
    This reverts commit 796dca81d8268cbb8606999869b008a89b647f50.

--- 10/10.cr
+++ 10/10.cr
@@ -5,11 +5,13 @@ numbers.push(numbers.max + 3)
 counts = numbers.each.cons_pair.map { |(x, y)| y - x }.tally
 puts "Part 1: #{counts[1] * counts[3]}"
 
+nexts = numbers[...-1].each_with_index.to_h do |(n, i)|
+  {n, numbers[i + 1..].take_while { |m| m - n <= 3 }}
+end
+
 possiblities = {numbers.last => 1_i64}
-numbers[...-1].reverse_each.with_index do |n, i|
-  p n, i
-  nexts = numbers[i + 1..].take_while { |m| m - n <= 3 }
-  possiblities[n] = nexts.sum { |m| possiblities[m] }
+nexts.keys.reverse_each do |n|
+  possiblities[n] = nexts[n].sum { |m| possiblities[m] }
 end
 
 puts "Part 2: #{possiblities[0]}"
